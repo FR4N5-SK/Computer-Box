bad37fdf64de3261d692246f9844f68d12240b24
FR4N5-SK <fran19062005@gmail.com>
2024-11-02 11:40:06 -0400

    Agregamos el proyecto base con tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        negro: "#000807",
+        blanco: "#FFFFFF",
+        "cool-gray": "#A2A3BB",
+        "tropical-indigo": "#9395D3",
+        "periwinkle": "#B3B7EE",
+        gris: "#D4D4D4"
+      },
+      fontFamily: {
+        Trocchi: ["Trocchi", "serif"],
+        "Source-sans-pro": ["Source Sans 3", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
